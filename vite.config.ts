0cfdcb684f1376704d9decf62236c7c29b1bf19b
wadahkode <mvp.dedefilaras@gmail.com>
2025-04-04 16:50:13 +0000

    support tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import AicssPlugin from './plugins/vite-plugin-aicss'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
     AicssPlugin({
       dir: 'aicss',
     }),
+    tailwindcss(),
   ],
 })
